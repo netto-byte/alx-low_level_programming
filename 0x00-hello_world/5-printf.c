566b05ce750299125f2018266e637e54daae2540
netto-byte <marvellousmatthew@gmail.com>
2023-09-10 17:16:47 +0100

    C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints types sizes
+ * main - prints a string using printf
  *
- * Return: Always 0 (Success)
+ * Return: Always (0)
  */
-int main(void)
-{
-	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of an int: %lu byte(s)\n", sizeof(int));
-	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
-	printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("Size of float: %lu byte(s)\n", sizeof(float));
+int main
+{	
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
-}
