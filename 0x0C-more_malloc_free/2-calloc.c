a72bb4d59529e7ee7ef419526622cf5b24e14376
netto-byte <marvellousmatthew@gmail.com>
2023-10-08 16:00:44 -0700

    Allocates memory for an array, using malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	len = nmemb * (size + 1);
+	len = nmemb * size;
 	mem = malloc(len);
 	if (!mem)
 		return (NULL);
