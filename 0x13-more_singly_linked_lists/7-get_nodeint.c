1eb76431c83b0a248ae9b918f00fc6be85b5f198
netto-byte <marvellousmatthew@gmail.com>
2023-10-25 11:15:17 -0700

    Returns the nth node of a listint_t linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,14 +8,14 @@
  *
  * Return: nth index of a listint_t.
  */
-listint_t *get_nodeit_at_index(listint_t *head, unsigned int index)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	size_t i;
 
 	for (i = 0; (i < index) && (head->next); i++)
 		head = head->next;
 
-	if (i < n)
+	if (i < index)
 		return (NULL);
 	return (head);
 }
