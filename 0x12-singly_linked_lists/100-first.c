1b9de627d66e18a5c3f11563ff8f31ae3de4e0d7
netto-byte <marvellousmatthew@gmail.com>
2023-10-25 06:01:01 -0700

    Prints a string before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void __attribute__((constructor)) hare(void);
+
+/**
+ * hare - Prints a string before the main function
+ */
+void hare(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore mu house upon my back!\n");
+}
