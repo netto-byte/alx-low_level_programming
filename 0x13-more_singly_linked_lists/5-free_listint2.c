e7c7685549ea71469fb20ff012074b56f013ee14
netto-byte <marvellousmatthew@gmail.com>
2023-10-25 08:53:22 -0700

    Fress a listint_t list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - Frees a listint_t list
+ * @head: A pointer to a listint_t list pointer
+ *
+ * Return: Nothing.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *p;
+
+	while (*head != NULL)
+	{
+		p = *head;
+		*head = (*head)->next;
+		free(p);
+	}
+	*head = NULL;
+}
