1e29a2e6758b63a29a61f49248d72bac8ad29045
netto-byte <marvellousmatthew@gmail.com>
2023-11-21 02:27:01 -0800

    Creates an array of chars, and initializes it with a special char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,6 +10,7 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
 	char *arr;
 
 	if (size == 0)
@@ -21,6 +22,11 @@ char *create_array(unsigned int size, char c)
 	if (arr == NULL)
 	{
 		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
 	}
 
 	return (arr);
