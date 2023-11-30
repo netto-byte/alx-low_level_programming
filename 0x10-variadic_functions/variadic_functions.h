7d050534390ff2118d6f070d3c66b56a7a00245e
netto-byte <marvellousmatthew@gmail.com>
2023-11-30 12:41:12 -0800

    Header file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,13 @@
+#ifndef _VAR_FUNC_H_
+#define _VAR_FUNC_H_
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *seperator, const unsigned int n, ...);
+void print_strings(const char *seperator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /* _VAR_FUNC_H_ */
