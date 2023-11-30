ab41d0cb137bf8ee794a9d09b1ef4a490f44da02
netto-byte <marvellousmatthew@gmail.com>
2023-11-30 12:01:24 -0800

    Prints numbers, followed by a new line

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Prints numbers passed as parameters,
+ * followed by a new line
+ * @n: Number of parametes to be passed.
+ *
+ * Return: void.
+ */
+void print_numbers(const char *seperator, const unsigned int n, ...)
+{
+	va_list arg_ptr;
+	size_t i;
+
+	va_start(arg_ptr, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(arg_ptr, int));
+		if (seperator && i < n - 1)
+			printf("%s", seperator);
+	}
+	printf("\n');
+}
