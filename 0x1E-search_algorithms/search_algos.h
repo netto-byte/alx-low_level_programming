b6ca8de3bcbd3bf6f44f49900211cf8cf027e8fb
netto-byte <marvellousmatthew@gmail.com>
2024-07-17 20:21:36 -0700

    Header File

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,5 +2,7 @@
 #define _SEARCH_ALGOS_H_
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
 #endif /* _SEARCH_ALGOS_H */
